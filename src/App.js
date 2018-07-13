c2d6a3be6c992f522629af206ad4b47719d98ae2
Rodrigo Banci <rblima0@gmail.com>
2018-07-13 14:06:30 -0300

    Layout do projeto ok

--- src/App.js
+++ src/App.js
@@ -2,30 +2,55 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { GOOGLEMAPS_API_KEY } from './utils/keys';
 import MapStyles from './utils/styles.json';
+import Logo from './styles/icons/logo.png';
 import './styles/css/App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<Map
-				google={this.props.google}
-				styles={MapStyles}
-				initialCenter={{
-					lat: -23.454315,
-					lng: -46.533652
-				}}
-				zoom={15}
-				onClick={this.onMapClicked}
-			>
-				<Marker onClick={this.onMarkerClick}
-                	name={'Localização atual'} />
- 
-				<InfoWindow onClose={this.onInfoWindowClose}>
-					<div>
-						<h1>Bem Vindo</h1>
+			<div className="content">
+				<main className="aside">
+					<div className="logo">
+						<img src={Logo} alt="Logotipo Neighborhood"></img>
 					</div>
-				</InfoWindow>
-			</Map>
+					<div className="location-filter">
+						<input type="text" placeholder="Preencha e filtre lugares"></input>
+					</div>
+					<nav>
+						<p>Mostrando 5 de 5 lugares</p>
+						<ul>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+							<li className="location-item">Localização 1</li>
+						</ul>
+					</nav>
+				</main>
+
+				<Map
+					google={this.props.google}
+					styles={MapStyles}
+					initialCenter={{
+						lat: -23.454315,
+						lng: -46.533652
+					}}
+					zoom={15}
+					onClick={this.onMapClicked}
+				>
+					<Marker onClick={this.onMarkerClick} name={'Localização atual'} />
+	
+					<InfoWindow onClose={this.onInfoWindowClose}>
+						<div>
+							<h1>Bem Vindo</h1>
+						</div>
+					</InfoWindow>
+				</Map>
+			</div>
 		);
 	}
 }
